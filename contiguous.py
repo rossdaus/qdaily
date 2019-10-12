9cecc8e1c260336bd5d003872a5f2e4c2c44c52d
rossdaus <rossdaus@example.com>
2019-10-12 17:14:33 +0300

    removed count variable

--- contiguous.py
+++ contiguous.py
@@ -1,19 +1,14 @@
 def solve(l, k):
     for n in range(len(l)):
-        total = 0
-        elements = l[n:]
-        count = 0
-        for x in elements:
+        total =  0
+        for x in range(len(l[n:])):
             if total == k:
-                return elements[:count]
+                return l[n:n + x]
             elif total > k:
                 break
-            total += x
-            count += 1
+            total += l[n:][x]
 
 l = [4, 6, 3, 6, 20, 30, 40, 5, 3, 2, 64, 34]
 k = 100
 answer = solve(l, k)
 print(answer)
-
-
