f83ac8106a4e969493aeda9f357b727c65640f9e
rossdaus <rossdaus@example.com>
2019-10-16 17:10:36 +1100

    Simplified some more

--- hops.py
+++ hops.py
@@ -2,25 +2,19 @@ def solve(a, pos=0, *path):
     """
     Find route to end of list a.
     pos is our current position (index of the array)
-    path is a tuple of values containing our previous positions (indexes)
+    path is a tuple of values containing our previous positions
     """
     # If at end of array we are successful, return the path we took
     if pos == len(a) - 1:
         return (*path, pos)
 
-    # get absolute of current hop value, fail if out of range
-    try:
-        hops = abs(a[pos])
-    except IndexError:
-        return False
-
-    # Fail if landed on a 0, or have been at this position
-    if hops == 0 or pos in path:
+    # Quit on repetition, out of range or hop value of 0
+    if pos in path or pos < 0 or pos + 1 > len(a) or a[pos] == 0:
         return False
 
     # Solve for forward and backward hops
-    return solve(a, pos + hops, *path, pos) or \
-           solve(a, pos - hops, *path, pos)
+    hops = abs(a[pos])
+    return solve(a, pos + hops, *path, pos) or solve(a, pos - hops, *path, pos)
 
 goods = (
     [-2, 0, -1, 0],
