6213624a199858b22ba1525e10db18d1a8d950fa
rossdaus <rossdaus@example.com>
2019-11-09 11:32:00 +1100

    optimised main loop

--- stockprices.py
+++ stockprices.py
@@ -3,25 +3,23 @@ import random
 
 def solve(market, k, best=None):
     """Get best outcome for k buys and sells using market history list."""
-
-    # get indices of all possible buy and sells
-    # and calculate profit for all buy/sell pairs
     k *= 2
+    # Get total profit for each combination of buys/sells
     for trade_times in combinations(range(len(market)), k):
-        prices = [market[i] for i in trade_times]
-        total_profit = sum((prices[i + 1] - prices[i] for i in range(0, k, 2)))
+        total_profit = sum((market[trade_times[i + 1]] -
+                            market[trade_times[i]]
+                            for i in range(0, k, 2)))
 
-        # check whether this is best so far
         if best is None or total_profit > best[0]:
-            buys = tuple(prices[i] for i in range(k) if i % 2 == 0)
-            sells = tuple(prices[i] for i in range(k) if i % 2 != 0)
-            trade_pairs = tuple(zip(buys, sells))
-            best = total_profit, trade_pairs
+            best = total_profit, trade_times
 
-    # show trades of best profitability
-    for x, y in best[1]:
+    total_profit, trade_times = best
+    prices = tuple(market[i] for i in trade_times)
+    buys = ((prices[i] for i in range(k) if i % 2 == 0))
+    sells = ((prices[i] for i in range(k) if i % 2 != 0))
+    for x, y in ((zip(buys, sells))):
         print(f"Buy at {x}, Sell at {y}        (Profit: {y - x})")
-    return best[0]
+    return total_profit
 
 k = 3
 market = [random.randint(0, 9) for _ in range(10)]
